700ef0d9c2efac98d6402c9663f05612527aac96
M3key <M3key@users.noreply.github.com>
2020-05-18 18:33:35 -0400

    changes
    new changes

--- src/App.js
+++ src/App.js
@@ -2,40 +2,45 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-class App extends React.Component{
-    state = { advice: ''};
+
+
+class App extends React.Component {
+    state = { advice: '' };
 
     componentDidMount() {
-      this.fetchAdvice();  
+      this.fetchAdvice();
     }
 
 fetchAdvice =() => {
-    axios.get("https://api.adviceslip.com/advice")
+  axios.get('https://api.adviceslip.com/advice')
     .then((response) => {
-        const {advice} = response.data.slip;
-        this.setState({advice});
+      const { advice } = response.data.slip;
+      this.setState({ advice });
     })
 
     .catch((error) => {
-        console.log(error);
-    })
+      console.log(error);
+    });
 }
-    render() {
-        const {advice} = this.state;
-        return(
-        <div className = "GuruAdvice">
-            <div className = "card">
-        <h1 className ="header">{advice}</h1>
+
+render() {
+  const { advice } = this.state;
+  return (
+    <div className="GuruAdvice">
+      <div className="card">
+        <h1 className="header">{advice}</h1>
         <button className="button" onClick={this.fetchAdvice}>
-            <span>
-                Advice me please!
-            </span>
+          <span>
+            Advice me please!
+          </span>
         </button>
-            </div>
-        </div>
-        )
-    }
+        <button className="button" id="tweetquote" onClick={event =>  window.location.href='https://twitter.com/intent/tweet?hashtags=&related=freecodecamp&text=' + encodeURIComponent('"' + advice)}>Thanks I should Tweet this!</button>
+      </div>
+    </div>
+
+  );
 }
-   
+}
+
 
 export default App;
